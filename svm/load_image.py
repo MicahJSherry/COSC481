904f3c9f8155bc9c606b7d21cbd699fd15385b51
Micah sherry <MicahJsherry@gmail.com>
2024-09-18 14:22:46 -0400

    changed load_images

--- svm/load_image.py
+++ svm/load_image.py
@@ -3,16 +3,18 @@ import cv2
 
 
 
-def load_images_(dir):
+def load_images(dir):
     images = []
     labels = []
     for root, dirs, files in os.walk(dir):
-        for file in files:
+        for file in files[0:100]:
             if file.endswith('.jpg'):
                 image_path = os.path.join(root, file)
-                image = cv2.imread(image_path) 
-
+                image = cv2.imread(image_path, mode=cv2.IMREAD_GRAYSCALE)
+                print(type(image))
                 images.append(image)
-                labels.append(root)
+                labels.append(root.split("/")[-1])
     
     return image, labels
+
+print(load_images("./spark22"))
