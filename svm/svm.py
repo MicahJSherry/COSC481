7044bdceb43ff8054487fd4eb439a7abdd382313
Micah sherry <MicahJsherry@gmail.com>
2024-09-18 15:24:54 -0400

    added train test split

--- svm/svm.py
+++ svm/svm.py
@@ -7,9 +7,11 @@ from sklearn.preprocessing import StandardScaler
 
 from load_image import load_images
 
-KERNALS = ['linear', 'poly', 'rbf', 'sigmoid']
+KERNALS = ['rbf']#, 'linear', 'poly',  'sigmoid']
 num_images = 100
 X, y = load_images("./spark22",num_images)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 svms    = {}
@@ -17,5 +19,7 @@ results = {}
 
 for kernal in KERNALS:
     svms[kernal] = make_pipeline(StandardScaler(), SVC(gamma='auto', kernel=kernal))
-    results[kernal]= svms[kernal].fit(X,y)
+    svms[kernal] = svms[kernal].fit(X_train,y_train)
+    results[kernal]= svms[kernal].predict(X_test)
+    
     print(results)
