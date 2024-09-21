0db336d655946220d74d0c6798befbef6508115a
Micah sherry <MicahJsherry@gmail.com>
2024-09-21 18:08:07 -0400

    updated to use PIL instead of cv2

--- kmeans/load_image.py
+++ kmeans/load_image.py
@@ -1,28 +1,23 @@
 import os 
-import cv2
 import numpy as np
+from PIL import Image
 
 from imgbeddings import imgbeddings
-ibed = imgbeddings()
 
 def load_images(dir,limit=100):
-    model = cv2.dnn.readNetFromCaffe('model.prototxt', 'model.caffemodel')
-    
-    images = []
     labels = []
+    paths = []
     for root, dirs, files in os.walk(dir):
         for file in files[0:limit]:
             if file.endswith('.jpg'):
                 image_path = os.path.join(root, file)
-                image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-                
-                image = cv2.resize(image,(224, 224))
+                paths.append(image_path)
 
-                embedding = ibed.to_embeddings(image)
-                print(embedding.shape)
-                images.append(images)
+    ibed = imgbeddings()
+
+    emb = ibed.to_embeddings(paths)
+
                 
-                labels.append(root.split("/")[-1])
     
-    return np.vstack(images), labels
+    return np.vstack(emb), labels
 
