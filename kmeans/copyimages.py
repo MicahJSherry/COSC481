baf71ef7f7c2e93abac9e0291af9dac7718cb068
Micah Sherry <MSherry@Millennial.software>
2024-09-21 23:56:31 -0400

    added small subset of images

--- kmeans/copyimages.py
+++ kmeans/copyimages.py
@@ -10,6 +10,6 @@ def copy_image_dir_subset(dir,num_files_per_dir, out_dir = "images"):
                 origin_path = os.path.join(root, file)
                 image = cv2.imread(origin_path)
             
-                cv2.imwrite(out_dir+file, image)
+                cv2.imwrite(out_dir+"/"+file, image)
 
-                
+copy_image_dir_subset("spark22/test", 2, out_dir = "images")                
