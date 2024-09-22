1fa01a56cbf5bc81c9d440e4b65315963bbfcf38
Micah sherry <MicahJsherry@gmail.com>
2024-09-21 23:36:35 -0400

    pivioted to image segmantation with kmeans

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,44 +1,22 @@
 from sklearn.cluster import KMeans
-
-import matplotlib.pyplot as plt
-
-
-from sklearn.metrics import confusion_matrix,  roc_curve, auc 
-from sklearn.metrics import classification_report
-from sklearn.preprocessing import LabelEncoder
-
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
-
-import numpy as np
-
-from load_image import load_images
-
-CLUSTERS = 11
-
-def plot_and_save(x, y, labels,title):
-    plt.figure()
-    plt.scatter(x, y,c=labels, cmap='viridis')
-    plt.title(title)
-    plt.savefig(title+".png")
-    plt.clf()
-
-le = LabelEncoder()
-num_images = 100
-X, y = load_images("./spark22",num_images)
-y = le.fit_transform(y)
+import cv2
 
 
-dim_reduction= { #"tsne_3d" : TSNE(n_components=3), 
-                 "tsne_2d" : TSNE(n_components=2), 
-                 #"pca_3d"  : PCA(n_components=2),
-                 "pca_2d"  : PCA(n_components=3) }
+image_path = "dog.png"
 
-reduced = { d: dim_reduction[d].fit_transform(X) for d in dim_reduction.keys() }
 
-k = KMeans(n_clusters=CLUSTERS)
-k.fit(X)
-labels = k.labels_ 
-for m, r in reduced.items():
-    plot_and_save(r[:,0], r[:,1], labels=labels, title=m+"_kmeans")
-    plot_and_save(r[:,0], r[:,1], labels=y, title=m+"_class")
+image = cv2.imread(image_path)
+original_shape = image.shape
+image = image.reshape(-1, image.shape[-1])
+print(original_shape)
+print(image.shape)
+
+kmeans = KMeans(n_clusters=50, n_init=100)
+kmeans.fit(image)
+
+image = kmeans.cluster_centers_[kmeans.labels_]
+image = image.reshape(original_shape)
+print(image)
+cv2.imwrite("color.png",image.astype("uint8") )
+
+
