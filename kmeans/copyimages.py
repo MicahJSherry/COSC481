84905fc0bafa37a529d71bc7e14de6359b2123db
Micah sherry <MicahJsherry@gmail.com>
2024-09-21 23:52:32 -0400

    create file for creating copy of images

---
+++ kmeans/copyimages.py
@@ -0,0 +1,15 @@
+import os 
+import cv2
+import numpy as np
+
+
+def copy_image_dir_subset(dir,num_files_per_dir, out_dir = "images"):
+    for root, dirs, files in os.walk(dir):
+        for file in files[0:num_files_per_dir]:
+            if file.endswith('.jpg'):
+                origin_path = os.path.join(root, file)
+                image = cv2.imread(origin_path)
+            
+                cv2.imwrite(out_dir+file, image)
+
+                
