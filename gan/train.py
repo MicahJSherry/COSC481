69a96526a1603b46468b832fa41d20102dbf43d9
Micah Sherry <MSherry@Millennial.software>
2024-11-26 18:49:26 -0500

    created basic files for gans

---
+++ gan/train.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from generator import make_generator_model
+
+generator = make_generator_model()
+
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
+
+
+plt.imsave('noise.png', generated_image, cmap='viridis') 
