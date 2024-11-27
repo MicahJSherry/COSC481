d70a5fa26a5de952045cf0a7f36e2d486f41703d
Micah Sherry <MSherry@Millennial.software>
2024-11-27 15:52:44 -0500

    retrained rgb with 1k epochs

--- gan/train.py
+++ gan/train.py
@@ -56,8 +56,8 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  discriminator=discriminator)
 
 OUT_DIR = "images/dcgan"
-EPOCHS = 100
-noise_dim = 100
+EPOCHS = 1000
+noise_dim = 1000
 num_examples_to_generate = 16
 
 # You will reuse this seed overtime (so it's easier)
